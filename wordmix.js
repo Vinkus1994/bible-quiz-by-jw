14d32b8b46af777bf7ba49f53785fff0050a388b
Vinkus1994 <vince.mira03@gmail.com>
2025-12-24 00:42:55 +0100

    Add files via upload

---
+++ wordmix.js
@@ -0,0 +1,65 @@
+/* wordmix.js — banques de mots pour le mode MOTS MÊLÉS */
+(() => {
+  window.WORDMIX_BANKS = [
+  {
+    "key": "at_1",
+    "title": "Livres 1",
+    "subtitle": "Ancien Testament",
+    "size": 14,
+    "words": [
+      "GENESE",
+      "EXODE",
+      "LEVITIQUE",
+      "NOMBRES",
+      "PSAUMES",
+      "JOSUE",
+      "JUGES",
+      "RUTH",
+      "SAMUEL",
+      "ROIS",
+      "CHRONIQUES"
+    ]
+  },
+  {
+    "key": "at_2",
+    "title": "Livres 2",
+    "subtitle": "Ancien Testament",
+    "size": 14,
+    "words": [
+      "ESDRAS",
+      "NEHEMIE",
+      "ESTHER",
+      "JOB",
+      "DEUTERONOME",
+      "PROVERBES",
+      "ECCLESIASTE",
+      "CHANTDESALOMON",
+      "ISAIE",
+      "JEREMIE",
+      "LAMENTATIONS",
+      "EZECHIEL"
+    ]
+  },
+  {
+    "key": "at_3",
+    "title": "Livres 3",
+    "subtitle": "Ancien Testament",
+    "size": 14,
+    "words": [
+      "DANIEL",
+      "OSEE",
+      "JOEL",
+      "AMOS",
+      "ABDIAS",
+      "JONAS",
+      "MICHEE",
+      "NAHUM",
+      "HABACUC",
+      "SOPHONIE",
+      "AGGEE",
+      "ZACHARIE",
+      "MALACHIE"
+    ]
+  }
+];
+})();
